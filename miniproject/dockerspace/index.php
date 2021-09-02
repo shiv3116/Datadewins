6b3daf486a90dabdca82b88046cc2f0180432d93
Shivam Sharma <60747392+shiv3116@users.noreply.github.com>
2021-09-02 15:06:06 +0530

    Update index.php

--- miniproject/dockerspace/index.php
+++ miniproject/dockerspace/index.php
@@ -4,7 +4,8 @@
 $host = 'localhost';
 $user = 'root';
 $pass = 'root';
-$conn = new mysqli($host, $user, $pass);
+$db = 'app_db';
+$conn = new mysqli($host, $user, $pass, $db);
 
 if($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
