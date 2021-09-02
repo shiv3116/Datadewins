7b81a2a2161915502d95eb2175526f35b6adf5cd
Shivam Sharma <60747392+shiv3116@users.noreply.github.com>
2021-09-02 13:12:29 +0530

    Update index.php

--- miniproject/dockerspace/index.php
+++ miniproject/dockerspace/index.php
@@ -1,7 +1,7 @@
 <h1>Hello Datadewins</h1>
 <h4>Attempting MYSQL Connection</h4>
 <?php
-$host = '127.0.0.1:3307';
+$host = 'localhost';
 $user = 'root';
 $pass = 'root';
 $conn = new mysqli($host, $user, $pass);
